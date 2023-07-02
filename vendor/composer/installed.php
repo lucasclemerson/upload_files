bb959aac9546dfc7b3616ad1bc973dc0202ac53b
lucasclemerson <clemerson.lucas.oliveira@gmail.com>
2023-07-02 14:52:30 -0300

    Novos arquivos

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => '__root__',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'b34871c83de1a9aa6aaeb2c6e7558ceba87ab542',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        '__root__' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'b34871c83de1a9aa6aaeb2c6e7558ceba87ab542',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
